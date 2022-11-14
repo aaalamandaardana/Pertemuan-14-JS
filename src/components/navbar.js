999742a51057c1d477216675214e251081204758
Alamanda Ardana <108120338+aaalamandaardana@users.noreply.github.com>
2022-11-14 17:13:52 +0700

    Program React JS Pertama (Sederhana; Awal Belajar)

---
+++ src/components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+         <>
+            <div className='navbar'>
+                <h1>PeTIK Jombang - 2022</h1>
+                <img src='https://petik.or.id/wp-content/uploads/2022/08/logo-petik.png'></img>
+            </div>
+         </>
+    )
+}
+
+export default Navbar
---
+++ src/components/pesan.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Pesan = () => {
+    return (
+        <>
+            <div className='pesan'>
+                <h1>Halo, Ini adalah Program React JS Pertama Saya</h1>
+            </div>
+        </>
+    );
+}
+
+export default Pesan;
